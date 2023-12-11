51be4bb00dc913cc5f77c35c148df2c302561fd3
lingulist <mattis.list@lingpy.org>
2023-12-11 16:20:30 +0100

    update new files

---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup
+
+
+setup(
+    name='cldfbench_chaomozhizhen',
+    py_modules=['cldfbench_chaomozhizhen'],
+    include_package_data=True,
+    zip_safe=False,
+    entry_points={
+        'cldfbench.dataset': [
+            'chaomozhizhen=cldfbench_chaomozhizhen:Dataset',
+        ]
+    },
+    install_requires=[
+        'cldfbench',
+        'pylexibank',
+    ],
+    extras_require={
+        'test': [
+            'pytest-cldf',
+        ],
+    },
+)
