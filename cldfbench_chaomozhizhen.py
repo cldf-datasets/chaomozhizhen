a364f20eef2da037892e1a4082d292199ac4606f
lingulist <mattis.list@lingulist.de>
2023-12-12 07:25:14 +0100

    update new files

--- cldfbench_chaomozhizhen.py
+++ cldfbench_chaomozhizhen.py
@@ -29,6 +29,109 @@ def is_chinese(name):
     return True
 
 
+def parse_data(data, text):
+    examples = defaultdict(
+            lambda : {
+            "ID": "",
+            "Number": 0,
+            "Slip_ID": "",
+            "Slip_Number": 0,
+            "Text_ID": text,
+            "Language_ID": "OldChinese",
+            "Primary_Text": [],
+            "Analyzed_Word": [],
+            "Old_Chinese_Reading": [],
+            "Middle_Chinese_Reading": [],
+            "Gloss": [],
+            "Word_IDS": [],
+            "Cognacy": ""})
+    entries = defaultdict(
+            lambda : {
+                "ID": "",
+                "Language_ID": "OldChinese",
+                "Headword": "",
+                "Character": "",
+                "Character_Variants": [],
+                "Middle_Chinese": "",
+                "Old_Chinese": "",
+                "Gloss": "",
+                "Glosses": [],
+                "Middle_Chinese_Readings": [],
+                "Old_Chinese_Readings": [],
+                "Example_IDS": []})
+    
+    slip_number, phrase_number, word_number = 0, 0, 0
+    slip_idx, phrase_idx, word_idx = "", "", ""
+    previous_slip, previous_phrase = "", ""
+    word2id, word_count = {}, 1
+    for row in data:
+        if previous_slip != row["Slip_ID"]:
+            word_number += 1
+            slip_number += 1
+            slip_idx = text + "-" + row["Slip_ID"]
+            previous_slip = row["Slip_ID"]
+        if previous_phrase != row["Phrase_ID"]:
+            phrase_number += 1
+            phrase_idx = text + "-" + row["Slip_ID"] + "-" + row["Phrase_ID"]
+            previous_phrase = row["Phrase_ID"]
+            # fill in entries
+            examples[phrase_idx]["ID"] = phrase_idx
+            examples[phrase_idx]["Slip_ID"] = slip_idx
+            examples[phrase_idx]["Number"] = phrase_number
+            examples[phrase_idx]["Slip_Number"] = slip_number
+
+
+        if row["Word"] not in word2id:
+            word2id[row["Word"]] = "word-{0}".format(word_count)
+            word_count += 1
+            word_idx = word2id[row["Word"]]
+            
+            # fill in basic entries
+            entries[word_idx]["ID"] = word_idx
+            entries[word_idx]["Headword"] = row["Word"] 
+            entries[word_idx]["Gloss"] = row["Gloss"]
+            entries[word_idx]["Middle_Chinese"] = row["MC"]
+            entries[word_idx]["Old_Chinese"] = row["OC"]
+            entries[word_idx]["Character"] = row["Word"]
+        
+        # fill word dictionary
+        entries[word_idx]["Character_Variants"] += [row["Raw_Word"]]
+        entries[word_idx]["Middle_Chinese_Readings"] += row["OC"].split(" // ")
+        entries[word_idx]["Old_Chinese_Readings"] += row["MC"].split(" ")
+        entries[word_idx]["Glosses"] += [row["Gloss"]]
+        entries[word_idx]["Example_IDS"] += [phrase_idx]
+
+        # fill text examples
+        examples[phrase_idx]["Old_Chinese_Reading"] += [row["OC"].split(" // ")[0]]
+        examples[phrase_idx]["Middle_Chinese_Reading"] += [row["MC"].split(" ")[0]]
+        examples[phrase_idx]["Primary_Text"] += [row["Raw_Word"]]
+        examples[phrase_idx]["Analyzed_Word"] += [row["Word"]]
+        examples[phrase_idx]["Gloss"] += [row["Gloss"].strip().replace(" ", ".")]
+        examples[phrase_idx]["Word_IDS"] += [word_idx]
+    
+    # refine data
+    for example in examples.values():
+        example["Primary_Text"] = " ".join(example["Primary_Text"])
+    for entry in entries.values():
+        entry["Middle_Chinese_Readings"] = sorted(
+                set(entry["Middle_Chinese_Readings"]),
+                key=lambda x: entry["Middle_Chinese_Readings"].count(x),
+                reverse=True)
+        entry["Old_Chinese_Readings"] = sorted(
+                set(entry["Old_Chinese_Readings"]),
+                key=lambda x: entry["Old_Chinese_Readings"].count(x),
+                reverse=True)
+        entry["Glosses"] = sorted(
+                set(entry["Glosses"]),
+                key=lambda x: entry["Glosses"].count(x),
+                reverse=True)
+
+
+                
+    
+    return entries, examples
+
+
 
 
 class Dataset(BaseDataset):
@@ -55,17 +158,24 @@ class Dataset(BaseDataset):
         """
         Convert the raw data to a CLDF dataset.
         """
-
+        
+        # language table (Old Chinese)
         args.writer.cldf.add_component('LanguageTable')
+        # entries, links to examples
         args.writer.cldf.add_component(
             "EntryTable",
+            {"name": "Segments", "datatype": "string", "separator": " "},
+            {"name": "Sound_Classes", "datatype": "string", "separator": " "},
             "Character",
             "Middle Chinese",
             "Old_Chinese",
-            {"name": "Gloss", "datatype": "string"},
-            {"name": "Example_ID", "datatype": "string"},
+            {"name": "Middle_Chinese_Readings", "datatype": "string", "separator": " "},
+            {"name": "Old_Chinese_Readings", "datatype": "string", "separator": " "},
+            {"name": "Glosses", "datatype": "string", "separator": " "},
+            {"name": "Example_IDS", "datatype": "string", "separator": " "},
             )
-
+        
+        # examples (glossed text)
         args.writer.cldf.add_component(
             'ExampleTable',
             {"name": 'Number', "datatype": "integer"},
@@ -73,7 +183,10 @@ class Dataset(BaseDataset):
             {'name': 'Slip_Number', 'datatype': 'integer'},
             'Text_ID',
             {"name": "Word_IDS", "datatype": "string", "separator": " "},
-            #{"name": "Word_Forms", "datatype": "string", "separator": r"\t"},
+            {"name": "Middle_Chinese_Reading", "datatype": "string", "separator": " "},
+            {"name": "Old_Chinese_Reading", "datatype": "string", "separator": " "},
+            "Cognacy"
+
         )
 
 
@@ -83,195 +196,24 @@ class Dataset(BaseDataset):
                 'ExampleTable', 'Text_ID', 'texts.csv', 'ID'
                 )
         args.writer.cldf.add_foreign_key(
-                "EntryTable", "Example_ID", "examples.csv", "ID")
+                "EntryTable", "Example_IDS", "examples.csv", "ID")
         
+        # fill language table
         args.writer.objects['LanguageTable'].append(
                 {
                     'ID': 'OldChinese', 
                     'Name': 'Old Chinese', 
                     'Glottocode': ''}
                 )
+        
+        texts = ["ad"]
 
-        # add a form table
-        args.writer.cldf.add_component(
-            "FormTable",
-            {"name": "Character", "datatype": "string"},
-            {"name": "Entry_IDS", "datatype": "string", "separator": " "},
-            {"name": "Middle_Chinese_Forms", "datatype": "string", "separator": " // "},
-            {"name": "Old_Chinese_Forms", "datatype": "string", "separator": " // "},
-            {"name": "Glosses", "datatype": "string", "separator": " // "},
-            )
-    
-        words = defaultdict(
-                lambda : {
-                    "Glosses": [], 
-                    "Phrase_IDS": [],
-                    "Word_Numbers": [],
-                    "Word_Forms": [],
-                    "Middle_Chinese": [],
-                    "Old_Chinese": [],
-                    "Word_IDS": []
-                    })
-        word2id = {}
-        data = self.raw_dir.read_csv("ad.tsv", dicts=True, delimiter="\t")
-        phrase = ""
-        word_count = 1
-        phrase_count = 1
+        for text in texts:
+            args.log.info('reading text {0}'.format(text))
+            data = self.raw_dir.read_csv(text+".tsv", delimiter="\t", dicts=True)
+            entries, examples = parse_data(data, text)
+            for entry in entries.values():
+                args.writer.objects["EntryTable"].append(entry)
+            for example in examples.values():
+                args.writer.objects["ExampleTable"].append(example)
 
-        phrases = defaultdict(lambda : {
-            "Word_IDS": [],
-            "Word_Forms": [],
-            "Raw_Forms": [],
-            "Gloss": []
-            })
-        for i, row in enumerate(data):
-
-            idx = row["ID"]
-            if phrase != row["Phrase_ID"]:
-                word_number = 1
-                phrase = row["Phrase_ID"]
-                phrase_idx = "ad-" + str(phrase)
-                phrases[phrase_idx]["Slip_ID"] = row["Slip_ID"]
-                phrases[phrase_idx]["Phrase_Number"] = phrase_count
-                phrase_count += 1
-            if not row["Word"] in word2id:
-                word_idx = word_count
-                word2id[row["Word"]] = word_idx
-                word_count += 1
-                words[word_idx]["Character"] = row["Word"]
-            else:
-                word_idx = word2id[row["Word"]]
-            
-            # fill in data on phrases
-            phrase_idx = "ad-" + str(phrase)
-            phrases[phrase_idx]["Word_IDS"] += ["word-ad-" + str(idx)]
-            phrases[phrase_idx]["Word_Forms"] += [row["Word"]]
-            phrases[phrase_idx]["Gloss"] += [row["Gloss"].replace(" ", "_")]
-            phrases[phrase_idx]["Raw_Forms"] += [row["Raw_Word"]]
-            
-            words[word_idx]["Glosses"] += [row["Gloss"]]
-            words[word_idx]["Word_Forms"] += [row["Raw_Word"]]
-            words[word_idx]["Middle_Chinese"] += row["MC"].split()
-            words[word_idx]["Old_Chinese"] += [
-                    x.strip("!") for x in row["OC"].split(" // ")]
-            words[word_idx]["Word_Numbers"] += [word_number]
-            words[word_idx]["Phrase_IDS"] += [phrase]
-            words[word_idx]["Word_IDS"] += [idx]
-
-            args.writer.objects["EntryTable"].append({
-                "ID": "ad-"+str(i+1),
-                "Language_ID": "OldChinese",
-                "Headword": row["Word"],
-                "Middle_Chinese": row["MC"],
-                "Old_Chinese": row["OC"],
-                "Gloss": row["Gloss"],
-                "Character": row["Raw_Word"],
-                "Example_ID": "ad-{0}".format(phrase)
-                })
-        for word, values in sorted(words.items()):
-            args.writer.objects["FormTable"].append({
-                "ID": "word-ad-{0}".format(word),
-                "Language_ID": "OldChinese",
-                "Form": values["Old_Chinese"][0],
-                "Character": values["Character"],
-                "Entry_IDS": values["Word_IDS"],
-                "Middle_Chinese_Forms": values["Middle_Chinese"],
-                "Old_Chinese_Forms": values["Old_Chinese"],
-                })
-        for phrase, values in sorted(
-                phrases.items(), key=lambda x: int(x[1]["Phrase_Number"])):
-            args.writer.objects["ExampleTable"].append({
-                "ID": phrase,
-                "Number": values["Phrase_Number"],
-                "Language_ID": "OldChinese",
-                "Slip_ID": values["Slip_ID"],
-                "Slip_Number": values["Slip_ID"],
-                "Text_ID": "ad",
-                "Word_IDS": values["Word_IDS"],
-                "Primary_Text": " ".join(values["Raw_Forms"]),
-                "Analyzed_Word": values["Word_Forms"],
-                "Gloss": values["Gloss"],
-                })
-
-
-        print(phrases)
-
-            #    poems_ = f.read().split("\n\n\n\n")
-            #    for poem in poems_:
-            #        in_poem = False
-            #        for row in poem.split("\n"):
-            #            if row.startswith("?"):
-            #                pass
-
-            #            elif row.strip() and row[0].isdigit():
-            #                name = row
-            #                in_poem = True
-            #                stanza = 1
-            #                P[name] = {stanza: []}
-            #            elif in_poem:
-            #                if row.strip():
-            #                    P[name][stanza] += [row.strip()]
-            #                else:
-            #                    stanza += 1
-            #                    P[name][stanza] = []
-        #args.log.info("parsed data")
-        #
-        #poems = {}
-        #idx = 1
-        #entries = defaultdict(list)
-        #for i, name in enumerate(P):
-        #    poem_id, poem_name = name.split(". ")
-        #    args.writer.objects["poems.csv"].append({
-        #        "ID": poem_id,
-        #        "Title": poem_name,
-        #        })
-        #    args.log.info("Analyzing poem {0} / {1}".format(i+1, name))
-        #    for stanza in P[name]:
-        #        for i, row in enumerate(P[name][stanza]):
-        #            for j, (
-        #                    phrase, chars, rhyme_words, rhyme_word_idxs,
-        #                    rhyme_idxs
-        #                    ) in enumerate(parse_line(row)):
-        #                for word in rhyme_words:
-        #                    entries[word] += [idx]
-        #                args.writer.objects["ExampleTable"].append({
-        #                    "ID": idx,
-        #                    "Primary_Text": phrase,
-        #                    "Analyzed_Word": chars,
-        #                    "Gloss": "",
-        #                    "Poem_ID": poem_id,
-        #                    "Stanza_Number": stanza,
-        #                    "Line_Number": i+1,
-        #                    "Phrase_Number": j+1,
-        #                    "Language_ID": "OldChinese",
-        #                    "Rhyme_Words": rhyme_words,
-        #                    "Rhyme_Word_Indices": rhyme_word_idxs ,
-        #                    "Rhyme_IDS": ["{0}-{1}".format(
-        #                        poem_id, rid) for rid in rhyme_idxs]
-        #                    })
-        #                idx += 1
-        #for i, (entry, occs) in enumerate(entries.items()):
-        #    args.writer.objects["EntryTable"].append({
-        #        "ID": i+1,
-        #        "Language_ID": "OldChinese",
-        #        "Headword": entry,
-        #        "Example_IDS": occs
-        #        })
-
-
-
-        # output
-
-        #args.writer.cldf.properties['dc:creator'] = "Johann-Mattis List" 
-
-        #language = {
-        #    'ID': "OldChinese",
-        #    'Name': "Old Chinese",
-        #    'Glottocode': "",
-        #}
-        #args.writer.objects['LanguageTable'] = [language]
-
-        #args.writer.objects['EntryTable'] = entries
-        #args.writer.objects['SenseTable'] = senses
-        #args.writer.objects['ExampleTable'] = examples
-        #args.writer.objects['media.csv'] = media
