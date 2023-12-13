a4cd63a6c7da7df279cfa35a446d6dc1c0922569
MichelePulini <125753111+MichelePulini@users.noreply.github.com>
2023-12-13 17:56:40 +0100

    Update cldfbench_chaomozhizhen.py

--- cldfbench_chaomozhizhen.py
+++ cldfbench_chaomozhizhen.py
@@ -418,7 +418,7 @@ class Dataset(BaseDataset):
 
         errors = "# Text - Table - Mismatches\n\n"
         for key, example in full_text.items():
-            if 100 < key < 110:
+            if 10 < key < 20:
                 errors += "## Unit {0}, phrase {1} in Text\n\n".format(
                         example["Unit"], key)
                 header_len = max(
